e18288d892d38f6280dbcb433f309dc8c6705f7d
pabli <pabli@PABLO>
2024-02-15 14:07:22 -0600

    Agregar archivos de proyecto.

---
+++ tarea2prograUsodetipodedatosNumericos/Program.cs
@@ -0,0 +1,46 @@
+﻿
+/*Console.WriteLine("introduce la base del triangulo: ");
+double baseTriangulo = double.Parse(Console.ReadLine());
+
+Console.WriteLine("introduce la altura del triangulo: ");
+double alturaTriangulo = double.Parse(Console.ReadLine());
+
+double areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+Console.WriteLine("el area del triangulo es: {0}", areaTriangulo);*/
+
+/*Console.WriteLine("introduce el radio de la esfera: ");
+ double radioEsfera = double.Parse(Console.ReadLine());
+ double volumenEsfera = (4.0 / 3.0) * Math.PI * Math.Pow(radioEsfera, 3);
+ Console.WriteLine("el volumen de la esfera es: {0}", volumenEsfera);*/
+
+
+/*Console.WriteLine("introduce la coordenada X del primer punto: ");
+double x1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("introduce la coordenada Y del primer punto: ");
+double y1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("introduce la coordenada X del segundo punto: ");
+double x2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("introduce la coordenada Y del segundo punto: ");
+double y2 = double.Parse(Console.ReadLine());
+
+double distancia = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+Console.WriteLine("la distancia entre los dos puntos es: {0}", distancia);*/
+
+
+
+/*Console.WriteLine("Introduce la temperatura en grados Celsius: ");
+double temperaturaCelsius = double.Parse(Console.ReadLine());
+double temperaturaFahrenheit = (temperaturaCelsius * 9 / 5) + 32;
+Console.WriteLine("La temperatura en grados Fahrenheit es: {0}", temperaturaFahrenheit);*/
+
+
+
+        /*Console.WriteLine("introduce la temperatura en grados Fahrenheit: ");
+        double temperaturaFahrenheit = double.Parse(Console.ReadLine());
+        double temperaturaCelsius = (temperaturaFahrenheit - 32) * 5 / 9;
+        Console.WriteLine("la temperatura en grados Celsius es: {0}", temperaturaCelsius);*/
+  
